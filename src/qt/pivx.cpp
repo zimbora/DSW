4e047189917f51d7140e4eb63ad5372074a2a869
João Silva <jsilva@thinkdigital.pt>
2023-10-20 15:36:55 +0100

    disables Q_INIT_RESOURCE

--- src/qt/pivx.cpp
+++ src/qt/pivx.cpp
@@ -543,7 +543,7 @@ int main(int argc, char* argv[])
 
 /// 2. Basic Qt initialization (not dependent on parameters or configuration)
     Q_INIT_RESOURCE(pivx_locale);
-    Q_INIT_RESOURCE(pivx);
+    //Q_INIT_RESOURCE(pivx);
 
     //TODO to set this up we need to review all the UI measurements to be relative to the screen DPI
     // Generate high-dpi pixmaps
